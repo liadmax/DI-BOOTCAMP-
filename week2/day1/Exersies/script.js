76327cded71a030148a58d932e8ad212a973ad5e
unknown <shapiraliad@gmail.com>
2022-06-26 12:50:27 +0300

    helloe

---
+++ week2/day1/Exersies/script.js
@@ -0,0 +1,21 @@
+
+//alert('hello');
+
+let favoriteFood = 'pizza';
+let favoritMealOfTheDay = 'dinner';
+console.log('i eat '+ favoriteFood+' at every '+favoritMealOfTheDay);
+ //exercise 1
+
+ let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+ let myWatchedSeriesLenght = myWatchedSeries.length;
+ let myWatchedSeriesSentence = ['black mirror', 'money heist', 'and the big bang theory'];
+ console.log('i watched 3 series'+ myWatchedSeriesSentence+ ' !');
+ //part one
+ myWatchedSeries [2]= 'frinds';
+ myWatchedSeries [0]= 'rick and morty';
+ let thirdSeries = myWatchedSeries[1];
+ console.log(thirdSeries);
+ console.log(thirdSeries.charAt(2));
+ console.log(myWatchedSeries);
+ //part two
+//exercise 2
