a7fffad16ce5dd2f0a1739f47118cd2bb4c75d1e
unknown <shapiraliad@gmail.com>
2022-06-26 13:29:26 +0300

    done exercise 1-6

--- week2/day1/Exersies/script.js
+++ week2/day1/Exersies/script.js
@@ -19,3 +19,151 @@ console.log('i eat '+ favoriteFood+' at every '+favoritMealOfTheDay);
  console.log(myWatchedSeries);
  //part two
 //exercise 2
+
+let celsius = 26;
+let fahrenheit =  Math.floor(celsius *(9/5) + 32);   ;
+console.log(fahrenheit);
+   //exercise 3
+
+   let c;
+   let a = 34;
+   let b = 21;
+
+    console.log(a+b) //first expression
+    // Prediction:55
+    // Actual:55
+
+    a = 2;
+
+    console.log(a+b) //second expression
+    // Prediction:23
+    // Actual   23
+
+    console.log(3 + 4 + '5');
+     // Prediction:7 ,'5' =75
+    // Actual 75 
+
+    //exercise 4
+
+    typeof(15)
+// Prediction:number
+// Actual:
+
+typeof(5.5)
+// Prediction:number
+// Actual:
+
+typeof(NaN)
+// Prediction:null
+// Actual:number
+
+typeof("hello")
+// Prediction:string
+// Actual:string
+
+typeof(true)
+// Prediction:boolean
+// Actual:boolean
+
+typeof(1 != 2)
+// Prediction:bollean
+// Actual:boolean
+
+"hamburger" + "s"
+// Prediction:hamburgers
+// Actual:
+
+"hamburgers" - "s"
+// Prediction:nan
+// Actual:nan
+
+"1" + "3"
+// Prediction:13
+// Actual:13
+
+"1" - "3"
+// Prediction:nan
+// Actual:-2
+
+"johnny" + 5
+// Prediction:johnny 5
+// Actual:johnny 5
+
+"johnny" - 5
+// Prediction:nan
+// Actual:nan
+
+99 * "hello"
+// Prediction:nan
+// Actual:nan
+
+1 != 1
+// Prediction:boolean
+// Actual:false
+
+1 == "1"
+// Prediction:true
+// Actual:true
+
+1 === "1"
+// Prediction:false
+// Actual:false
+//exercise 5
+
+
+5 + "34"
+// Prediction:534
+// Actual:534
+
+5 - "4"
+// Prediction:1
+// Actual:1
+
+10 % 5
+// Prediction:2
+// Actual:2
+
+5 % 10
+// Prediction:5
+// Actual:5
+
+"Java" + "Script"
+// Prediction:javascript
+// Actual:javascript
+
+" " + " "
+// Prediction:''
+// Actual:''
+
+" " + 0
+// Prediction:''0
+// Actual:' 0' 
+
+true + true
+// Prediction:nan
+// Actual:2
+
+true + false
+// Prediction:1
+// Actual:1
+
+false + true
+// Prediction:1
+// Actual:1
+
+false - true
+// Prediction:-1
+// Actual:-1
+
+!true
+// Prediction:false
+// Actual:false
+
+3 - 4
+// Prediction: -1
+// Actual:-1
+
+"Bob" - "bill"
+// Prediction:nan
+// Actual:nan
+ // exercise 6
