bedd737805acd881a588db8ccab77078bcdd3388
unknown <shapiraliad@gmail.com>
2022-06-26 14:13:59 +0300

    done2

---
+++ week2/day1/DailyChallenge/script.js
@@ -0,0 +1,33 @@
+//xp gold
+
+let sentence = ["my","favorite","color","is","blue"];
+console.log(sentence);
+//exercise 1
+
+let str1 = "mix";
+let str2 = "pod";
+let newWord = str1 +' '+ str2;
+console.log(newWord);
+// exercise 2
+
+let num1 = 2;
+let num2 = 3;
+alert(num1 + num2);
+	//exercise 3
+
+	let num3 = '2';
+	let num4 ='3';
+	let add =num3 + num4; 
+	let subtract1 =num3 - num4;
+	let subtract2 = num4 - num3;
+	let multiply = num3 * num4;
+	let divide1 =  num3 % num4;
+	let divide2 =num4 % num3;
+	console.log(add);
+	console.log(subtract1);
+	console.log(subtract2);
+	console.log(multiply);
+	console.log(divide1);
+	console.log(divide2);
+	
+	//bonus
