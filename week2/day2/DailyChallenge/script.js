2159a260f8bc3eadf4360e05dcc51555b6292243
unknown <shapiraliad@gmail.com>
2022-06-26 14:43:18 +0300

    w2 d2 not finished

---
+++ week2/day2/DailyChallenge/script.js
@@ -0,0 +1,11 @@
+let sentence =['The',' movie',' is',' not',' that',' bad,',' i',' like',' it'];
+let wordNot = 3;
+let wordBad = 5;
+
+
+if(wordBad > wordNot){
+	sentence[3]= ''
+	sentence [4]=''
+	sentence [5]= 'good'
+	console.log(sentence)
+};
