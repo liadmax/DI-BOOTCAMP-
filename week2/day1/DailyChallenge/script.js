5e6e80a46001986a1b83606da52e33775d939fe8
unknown <shapiraliad@gmail.com>
2022-06-27 08:40:57 +0300

    som new

--- week2/day1/DailyChallenge/script.js
+++ week2/day1/DailyChallenge/script.js
@@ -12,22 +12,37 @@ console.log(newWord);
 
 let num1 = 2;
 let num2 = 3;
-alert(num1 + num2);
+//alert(num1 + num2);
 	//exercise 3
+	console.log('BONOUS');
 
-	let num3 = '2';
-	let num4 ='3';
+	let num3 = 2 ;
+	let num4 = 3;
 	let add =num3 + num4; 
 	let subtract1 =num3 - num4;
 	let subtract2 = num4 - num3;
 	let multiply = num3 * num4;
 	let divide1 =  num3 % num4;
 	let divide2 =num4 % num3;
-	console.log(add);
-	console.log(subtract1);
-	console.log(subtract2);
-	console.log(multiply);
-	console.log(divide1);
-	console.log(divide2);
-	
+	let userReqest = 'add'
+	if(userReqest = 'add'){
+		console.log(add);
+	}else if (userReqest ='subtract num3 by num4') {
+
+		console.log(subtract1)
+	}else if (userReqest ='subtract num4 by num3') {
+
+		console.log(subtract2)
+	}else if (userReqest= 'multiply') {
+		
+		console.log(multiply)
+	}else if (userReqest ='divide num3 by num4') {
+		
+
+		console.log(divide1)
+	}else if (userReqest ='divide num4 by num3') {
+		
+
+		console.log(divide2);
+	}
 	//bonus
---
+++ week2/day2/Exersies/script.js
@@ -0,0 +1,38 @@
+let x = 5;
+let y = 2;
+
+if(x > y){
+	console.log('i was right!')
+}else {
+	console.log('i was wrong!')
+};
+//exercise1
+
+let newDog ='Chihuahua';
+console.log(newDog.length);
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+if(newDog ='Chihuahua'){
+	console.log('I love Chihuahuas, it’s my favorite dog breed')
+}else {
+	console.log('i dont care i prefer cats')
+};
+//exercise2
+let z =10
+if (z % 2 == 0){
+	console.log('z is an even number')
+}else{
+	console.log('z is an odd number')
+};
+//exercise3
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+let userName = 
+if(users = 0){
+	console.log('no one is online')
+}else if (users = 1) {
+	console.log(users + ' is online')
+	
+}else if (user <= 2) {
+	console.log(users + ' are online')
+	
+};
