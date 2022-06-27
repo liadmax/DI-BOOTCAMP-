7ec8aa418ce293e4a7951af3c9e1d158e98e5464
unknown <shapiraliad@gmail.com>
2022-06-27 07:16:56 +0300

    done w2d2 daily project

--- week2/day2/DailyChallenge/script.js
+++ week2/day2/DailyChallenge/script.js
@@ -1,11 +1,13 @@
-let sentence =['The',' movie',' is',' not',' that',' bad,',' i',' like',' it'];
+let sentence =['The',' movie',' is',' not',' that',' bad,',' i',' liked',' it'];
 let wordNot = 3;
 let wordBad = 5;
 
 
 if(wordBad > wordNot){
-	sentence[3]= ''
-	sentence [4]=''
+	sentence[3]= null
+	sentence [4]=null
 	sentence [5]= 'good'
+	console.log(sentence)
+}else {
 	console.log(sentence)
 };
