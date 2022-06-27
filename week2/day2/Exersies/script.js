e3a083863cba0d4bfa959f0e4b5e7fd7c7c5ccdb
unknown <shapiraliad@gmail.com>
2022-06-27 11:59:38 +0300

    exercise done

--- week2/day2/Exersies/script.js
+++ week2/day2/Exersies/script.js
@@ -18,11 +18,11 @@ if(newDog ='Chihuahua'){
 	console.log('i dont care i prefer cats')
 };
 //exercise2
-let z =10
+let z = prompt('select a number');
 if (z % 2 == 0){
-	console.log('z is an even number')
+	console.log(z +' is an even number')
 }else{
-	console.log('z is an odd number')
+	console.log(z +' is an odd number')
 };
 //exercise3
 let users = [ "Lea123", "Princess45", "cat&doglovers", "helooo@000"];
