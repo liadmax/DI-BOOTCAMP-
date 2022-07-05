16e676858d7a8679dc32606b7dc332a857b604d5
unknown <shapiraliad@gmail.com>
2022-07-05 10:49:19 +0300

    not done

---
+++ week3/day2/Exersies/script.js
@@ -0,0 +1,14 @@
+/*let div =document.querySelector('artical,h1');
+console.log(div);
+let p =document.querySelectorAll('P')
+p[p.length - 1].remove()
+
+function mYfunction(){
+	document.getElementById('reel').style.backgroundColor = 'red'
+}
+function hide(){
+	document.getElementById('demo').style.display = 'none'
+}
+function bold(){
+	document.getElementById("bold").style.fontWeight = "bold";
+}*/
---
+++ week3/day3/Exersies/script.js
@@ -0,0 +1,5 @@
+function sayHi(sale) {
+  alert( sale );
+}
+
+setTimeout(sayHi, 5000, 'The sales end in 10min !'); //  calls sayHi() after one second --> Hello, John
