d9e39a644bb0ba2ec13c16a887ed4110843093c0
unknown <shapiraliad@gmail.com>
2022-07-06 19:58:58 +0300

    ksd

---
+++ week3/day4/Exersies/script.js
@@ -0,0 +1,13 @@
+function hello() {
+  alert('hello world')
+
+}
+setTimeout(hello, 2000);
+function add(){
+  document.getElementById('container').innerHTML = ' <p> hello world</p>';
+
+}
+setTimeout(add, 2000);
+
+
+
