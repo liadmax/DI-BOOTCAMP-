344f4cff3e5f976460bfecc188fa3827494cbf08
unknown <shapiraliad@gmail.com>
2022-07-31 20:33:58 +0300

    done xp

---
+++ week7/day1/Exersies/npm-node/main.js
@@ -0,0 +1,25 @@
+let y = 5;
+let z = 10;
+goodLife = true;
+function greeting(){
+    if (goodLife = true){
+        console.log(`hello you are ${y+z}`)
+    }else{
+        console.log('my life suck')
+    }
+}
+greeting()
+// console.log(`hello you are ${a+b}`)
+//exercise 1
+console.log('Hi, thanks for running me with npm start! i hate node.js and my life')
+//exercise 2
+let a = 5;
+let b = 10;
+let x;
+
+let c = 6;
+x = a + b;
+console.log(`first run${x} `)
+console.log(`second run ${x+c}`)
+
+//exercise 3 
