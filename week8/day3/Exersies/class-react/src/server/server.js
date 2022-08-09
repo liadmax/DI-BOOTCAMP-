973fbcd22639c193fde9377527dd7d04f0497d98
unknown <shapiraliad@gmail.com>
2022-08-09 11:34:58 +0300

    sdh

---
+++ week8/day3/Exersies/class-react/src/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const knex =require('knex');
+
+const db =knex()
+client:'pg',
+connection{
+ 
+host:'ec2-34-231-177-125.compute-1.amazonaws.com'
+Database:'d32edgogguu9cc'
+User:'momjliqegvhsfx'
+Port:5432
+Password:'170c4585a04f6f9459354a0e30489236c168b76ffd2'
+ssl:{rejectUnauthorized:false}   
+};
